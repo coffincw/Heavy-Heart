97ae463b2d49f5f31817023e6d34b08381dc554e
V-Neigh <44106485+V-Neigh@users.noreply.github.com>
2018-10-20 17:33:39 -0400

    why python, why

--- SentimentAnalyzer.py
+++ SentimentAnalyzer.py
@@ -98,23 +98,23 @@ def main(query):
     # picking positive tweets from tweets
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
     # percentage of positive tweets
-    print("Positive tweets percentage: {} %".format(100 * len(ptweets) / len(tweets)))
+    # print("Positive tweets percentage: {} %".format(100 * len(ptweets) / len(tweets)))
     # picking negative tweets from tweets
     ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
     # percentage of negative tweets
-    print("Negative tweets percentage: {} %".format(100 * len(ntweets) / len(tweets)))
+    #print("Negative tweets percentage: {} %".format(100 * len(ntweets) / len(tweets)))
     # percentage of neutral tweets
-    print("Neutral tweets percentage: {} % \ ".format(100 * (len(tweets)-len(ntweets)-len(ptweets)) / len(tweets)))
+    #print("Neutral tweets percentage: {} % \ ".format(100 * (len(tweets)-len(ntweets)-len(ptweets)) / len(tweets)))
 
     # printing first 5 positive tweets
-    print("\n\nPositive tweets:")
-    for tweet in ptweets[:10]:
-        print(tweet['text'])
+    #print("\n\nPositive tweets:")
+    #for tweet in ptweets[:10]:
+    #    print(tweet['text'])
 
     # printing first 5 negative tweets
-    print("\n\nNegative tweets:")
-    for tweet in ntweets[:10]:
-        print(tweet['text'])
+    #print("\n\nNegative tweets:")
+    #for tweet in ntweets[:10]:
+    #    print(tweet['text'])
 
     return ntweets
 
