c73d49e8cfbdef75400abc8fe622401d88f3f96d
V-Neigh <44106485+V-Neigh@users.noreply.github.com>
2018-10-20 17:15:44 -0400

    Changes + getTimeline for creating training data

--- GetTimeline.py
+++ GetTimeline.py
@@ -1,31 +1,44 @@
 import re
 import tweepy
+import SentimentAnalyzer
 from tweepy import OAuthHandler
 from textblob import TextBlob
-from SentimentAnalyzer import TwitterClient
 
-class TimelineGetter(object):
-  def __init__(self, *args, **kwargs):
-    consumer_key = 'n4zRm3xuVoYoBHDLbSSqCxlII'
-    consumer_secret = 'i2bxYM53rVNv0NFnO4iALdUYUDXHSEp9JARjpSu6290B5W6BLj'
-    access_token = '3265727682-sWioD71Vv1zJie1KCERyHZWgzCdDsGAy3lzdLJA'
-    access_token_secret = 'Ir9vMlPyiJwafcqXOiRJgtdWpev2VU3rI0Z2MWXlJ79SV'
+def __init__(self, *args, **kwargs):
+  consumer_key = 'n4zRm3xuVoYoBHDLbSSqCxlII'
+  consumer_secret = 'i2bxYM53rVNv0NFnO4iALdUYUDXHSEp9JARjpSu6290B5W6BLj'
+  access_token = '3265727682-sWioD71Vv1zJie1KCERyHZWgzCdDsGAy3lzdLJA'
+  access_token_secret = 'Ir9vMlPyiJwafcqXOiRJgtdWpev2VU3rI0Z2MWXlJ79SV'
+   # attempt authentication
+  try:
+    # create OAuthHandler object
+    self.auth = OAuthHandler(consumer_key, consumer_secret)
+    # set access token and secret
+    self.auth.set_access_token(access_token, access_token_secret)
+    # create tweepy API object to fetch tweets
+    self.api = tweepy.API(self.auth)
+  except:
+    print("Error: Authentication Failed")
+  return super().__init__(*args, **kwargs)
 
-    # attempt authentication
-    try:
-      # create OAuthHandler object
-      self.auth = OAuthHandler(consumer_key, consumer_secret)
-      # set access token and secret
-      self.auth.set_access_token(access_token, access_token_secret)
-      # create tweepy API object to fetch tweets
-      self.api = tweepy.API(self.auth)
-    except:
-      print("Error: Authentication Failed")
-    return super().__init__(*args, **kwargs)
+def getTraining():
+  tweets = SentimentAnalyzer.main("#depressed")
+  anxiety = SentimentAnalyzer.main("#anxiety")
+  tweets.extend([x for x in anxiety if x not in tweets])
+  training = []
+  for x in tweets:
+    training.append(list(x.values())[0])
 
-  def getTweets():
-    tc = TwitterClient()
-    tweets = tc.
+  for x in training:
+    print(x)
+
+if __name__ == "__main__":
+    getTraining()
+
+
+
+
+
 
 
 
